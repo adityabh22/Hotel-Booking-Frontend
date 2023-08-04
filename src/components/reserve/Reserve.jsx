8cea8407dcda864e15385b39d53a988f5d891662
Aditya Bharadwaj <aditya.2002bh@gmail.com>
2023-08-04 22:14:49 +0530

    new one commit

--- src/components/reserve/Reserve.jsx
+++ src/components/reserve/Reserve.jsx
@@ -55,7 +55,7 @@ const Reserve = ({ setOpen, hotelId }) => {
     try {
       await Promise.all(
         selectedRooms.map((roomId) => {
-          const res = axios.put(`/rooms/availability/${roomId}`, {
+          const res = axios.put(`https://hotel-booking-mern-site.onrender.com/api/rooms/availability/${roomId}`, {
             dates: alldates,
           });
           return res.data;
